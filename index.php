9b32f6a814732b4b4b973a09603e4a548d70de9f
Oricy <d.duvernois@student.isartdigital.com>
2018-04-25 12:40:46 +0200

    add index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+</head>
+<body>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</body>
+</html>
