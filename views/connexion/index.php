8ab7bd0636b89962b364a2b9e66f2a074ae2cb1b
Mehditeranee <gallois.mehdi@gmail.com>
2018-04-26 10:32:58 +0200

    (mehdi) view > connexion

---
+++ views/connexion/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehdi
+ * Date: 26/04/2018
+ * Time: 10:31
+ */
+?>
+<div class="main-content blc">
+  <div class="ctn">
+
+    <h1>Connexion</h1>
+
+<form action="" method="post" class="form">
+    <fieldset>
+        <legend>Connexion</legend>
+
+        <label>Nom d'utilisateur&nbsp;:</label>
+        <input type="text" name="username">
+
+        <label>Mot de passe&nbsp;:</label>
+        <input type="password" name="password">
+
+        <input type="submit">
+    </fieldset>
+</form>
+
+<p>Pas encore inscrit ? <a href="/inscription">Cliquez ici pour corriger cela !</a></p>
+
+</div>
+</div>
