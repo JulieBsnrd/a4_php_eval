2446e7fc0210572edeca6a0fac2a09546d1a4017
Mehditeranee <gallois.mehdi@gmail.com>
2018-04-27 11:59:01 +0200

    (mehdi) wip gestion des salles
    update with Class

--- controllers/admin/AdminSalleCtrl.php
+++ controllers/admin/AdminSalleCtrl.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
 
-require '../../config/config.php';
-require '../../models/SallesModel.php';
+require 'config/config.php';
+require 'models/Salle.php';
 
 if($_SESSION['membre']['statut'] != "1"){
-	header("location:SignInCtrl.php");
+    header("location:SignInCtrl.php");
 }
 
 if(!empty($_GET)){
-	if(isset($_GET['action']) && $_GET['action'] == "delete"){
-		if(isset($_GET['id']) && !empty($_GET['id'])){
-			$salle = delete($db, $_GET['id']);
-			header('Location: AdminSalleCtrl.php');
-		}
-	}
+    if(isset($_GET['action']) && $_GET['action'] == "delete"){
+        if(isset($_GET['id']) && !empty($_GET['id'])){
+            $salle = Salle::delete($_GET['id']);
+            header('Location: AdminSalleCtrl.php');
+        }
+    }
 }
 
 if(!empty($_POST)){
-	if(isset($_POST['action']) && $_POST['action'] == "edit"){
-		if(isset($_POST['id']) && !empty($_POST['id'])){
-		    $titre = $_POST['titre'];
+    if(isset($_POST['action']) && $_POST['action'] == "edit"){
+        if(isset($_POST['id']) && !empty($_POST['id'])){
+            $titre = $_POST['titre'];
             $description = $_POST['description'];
             $pays = $_POST['pays'];
             $ville = $_POST['ville'];
@@ -28,11 +28,11 @@ if(!empty($_POST)){
             $cp = $_POST['cp'];
             $capacite = $_POST['capacite'];
             $categorie = $_POST['categorie'];
-            $salle = update($db, $_POST['id']);
-			header('Location: AdminSalleCtrl.php');
-		}
-	}
-	if(isset($_POST['action']) && $_POST['action'] == "create"){
+            $salle = Salle::update($_POST['id']);
+            header('Location: AdminSallesCtrl.php');
+        }
+    }
+    if(isset($_POST['action']) && $_POST['action'] == "create"){
         $titre = $_POST['titre'];
         $description = $_POST['description'];
         $pays = $_POST['pays'];
@@ -41,33 +41,32 @@ if(!empty($_POST)){
         $cp = $_POST['cp'];
         $capacite = $_POST['capacite'];
         $categorie = $_POST['categorie'];
-        $salle = create($db);
+        $salle = Salle::create();
         //Handle image from files
-        if(isset($_FILES['avatar']) && $_FILES['avatar']['error']==0)
+        if(isset($_FILES['photo']) && $_FILES['photo']['error']==0)
         {
 
-            header('Location: ProfilCtrl.php');
+            header('Location: profil.php');
 
         }
         else{
-            header('Location: SignInCtrl.php');
+            header('Location: signIn.php');
 
 
         }
-	}
+    }
 }
 
 
 
-if(isset($_GET['id']) && !empty($_GET['id']) && !empty($_GET['action']) && $_GET['action'] == "get"){
-	$salle = getOne($db, $_GET['id']);
-	include '../../views/admin/editionSalle.php';
+if (isset($_GET['id']) && !empty($_GET['id']) && !empty($_GET['action']) && $_GET['action'] == "get") {
+    $salle = Salle::find($_GET['id']);
+    include '../../views/admin/editionSalle.php';
+} elseif (isset($_GET['path']) && $_GET['path'] == "create") {
+    include '../../views/admin/ajoutSalle.php';
+} else {
+    $produits = Salle::all();
+    include '../../views/admin/gestionSalles.php';
 }
-elseif(isset($_GET['path']) && $_GET['path'] == "create"){
-	include '../../views/admin/ajoutSalle.php';
-}
-else{
-	$salles = getAll($db);
-	include '../../views/admin/gestionSalles.php';
-}
+
 ?>
