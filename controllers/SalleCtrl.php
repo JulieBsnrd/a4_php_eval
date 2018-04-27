028e95d0ac2576c12f19c44315d4ae0ece5c3e49
Oricy <d.duvernois@student.isartdigital.com>
2018-04-27 09:49:24 +0200

    file upload

--- controllers/SalleCtrl.php
+++ controllers/SalleCtrl.php
@@ -5,6 +5,23 @@ require '../config/config.php';
 
 require('../models/SallesModel.php');
 
+//Handle image from files
+/*if(isset($_FILES['avatar'])&&$_FILES['avatar']['error']==0)
+{
+	if($_FILES['avatar']['size']<=120000)
+	{
+		$ext = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'),1));
+		if($ext=="jpg" || $ext=="png" || $ext=="gif")
+		{
+			$tailleimage = getimagesize($_FILES['avatar']['tmp_name']);
+			if($tailleimage[0] < 150 && $tailleimage[1] < 150){
+				move_uploaded_file($_FILES['avatar']['tmp_name'],$_FILES['avatar']['name']);
+				$image = $_FILES['avatar']['name'];
+			}
+		}
+	}
+}*/
+
 if (isset($_GET['id']) && $_GET['id'] > 0) {
 	$salle = getOne($db, $_GET['id']);
 	
